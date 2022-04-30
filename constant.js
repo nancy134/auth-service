949bbfc0df201f03f95a186d2b4fcd011c35f618
Nancy Piedra <nancy_piedra@yahoo.com>
2022-04-30 14:01:34 +0000

    new cc api

--- constant.js
+++ constant.js
@@ -1,14 +1,26 @@
 const axios = require('axios');
 const utilities = require('./utilities');
+function makeid(length) {
+    var result           = [];
+    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for ( var i = 0; i < length; i++ ) {
+      result.push(characters.charAt(Math.floor(Math.random() *  charactersLength)));
+   }
+   return result.join('');
+}
 
 exports.getAuthorizationUrl = function(clientId){
+    var state = makeid(6);
     return new Promise(function(resolve, reject){
-        var baseUrl = "https://api.cc.email/v3/idfed";
+        var baseUrl = "https://authz.constantcontact.com/oauth2/default/v1/authorize";
         var authUrl = 
             baseUrl + 
             "?client_id=" + 
             clientId +
-            "&scope=contact_data+campaign_data+account_read&response_type=code" +
+            "&state=" +
+            state +
+            "&scope=contact_data+campaign_data+account_read+offline_access&response_type=code" +
             "&redirect_uri=";
 
         resolve(authUrl); 
@@ -16,7 +28,7 @@ exports.getAuthorizationUrl = function(clientId){
 }
 exports.getAccessToken = function(code, redirect_uri, clientId){
     return new Promise(function(resolve, reject){
-        var baseUrl = "https://idfed.constantcontact.com/as/token.oauth2";
+        var baseUrl = "https://authz.constantcontact.com/oauth2/default/v1/token";
         var authUrl = 
             baseUrl +
             "?code=" +
@@ -31,28 +43,30 @@ exports.getAccessToken = function(code, redirect_uri, clientId){
         var headers = {
             "Authorization": authHeader
         };
+
         var options = {
            url: authUrl,
            method: 'POST',
            headers: headers
         };
+
         axios(options).then(function(result){
             resolve(result);
         }).catch(function(err){
-            reject(utilities.processAxiosError(err));
+            var error = utilities.processAxiosError(err);
+            reject(error);
         });
     });
 }
 
 exports.refreshToken = function(query, clientId){
     return new Promise(function(resolve, reject){
-        var baseUrl = "https://idfed.constantcontact.com/as/token.oauth2";
+        var baseUrl = "https://authz.constantcontact.com/oauth2/default/v1/token";
         var refreshUrl =
             baseUrl + 
             "?" + query +
             "&grant_type=refresh_token";
         var auth = clientId + ":" + process.env.CC_SECRET;
-        console.log(auth);
         var authBase64 = Buffer.from(auth).toString('base64');
         var authHeader = "Basic " + authBase64;
 
