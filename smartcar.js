c4c46503f40688957952f0450aa51e5962cdcf0a
Bill Piedra <Bill@Bills-iMac-770.local>
2023-01-27 07:30:07 -0500

    Get Fuel

--- smartcar.js
+++ smartcar.js
@@ -28,9 +28,11 @@ exports.getAuthorizationUrl = function(){
         baseUrl +
         "response_type=code" +
         "&client_id=" + client.clientId +
-        "&scope=read_odometer read_vehicle_info read_vin read_battery read_odometer read_tires required:read_location" +        "&redirect_uri=" + client.redirectUri +
-        "&state=0facda3q3q3q3q19" +
+        "&scope=read_odometer read_vehicle_info read_vin read_battery read_odometer read_tires read_fuel required:read_location" +        "&state=0facda3q3q3q3q19" +
+
+        "&redirect_uri=" + client.redirectUri +
         "&mode=test"
+
         resolve(authUrl);
     });
 }
